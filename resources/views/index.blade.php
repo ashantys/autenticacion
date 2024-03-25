063341fccccba57f5b69b4bbf489f2b9a3accded
Ashanty Sierra <ashanty.sierram@gmail.com>
2024-03-25 12:32:59 -0600

    feat(login y register)se finalizo el diseño de la pagina de login y register

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,9 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Welcome!</title>
 </head>
-    <body class="bg-gradient">
+    <body class="bg-gradient-app">
         <div class="relative min-h-screen flex flex-col items-center justify-center">
-            <div class="bg-cyan-950 shadow-md rounded-lg p-6 w-80 h-60 border-cyan-600">
+            <div class="border-solid border border-cyan-600 shadow-xl sm:rounded-lg p-6 w-80 h-60">
                 <h1 class="text-3xl text-cyan-50 font-semibold mb-5">Welcome!</h1>
                 <h2 class="text-xl text-cyan-50 mb-10"> Join us today and enjoy our amazing app!</h2>
                     <div class="relative flex flex-col items-baseline">
