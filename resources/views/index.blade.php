795a59c76d75e4e80b3dd615709c6625f0ec6df6
Ashanty Sierra <ashanty.sierram@gmail.com>
2024-03-25 11:24:05 -0600

    feat(index)se finalizo la index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Welcome!</title>
 </head>
-    <body>
+    <body class="bg-gradient">
         <div class="relative min-h-screen flex flex-col items-center justify-center">
-            <div class="bg-cyan-900 shadow-md rounded-lg p-6 w-80 h-60">
-                <h1 class="text-2xl text-cyan-100 font-semibold mb-4">Welcome</h1>
-                    <div class="relative flex flex-col items-start">
+            <div class="bg-cyan-950 shadow-md rounded-lg p-6 w-80 h-60 border-cyan-600">
+                <h1 class="text-3xl text-cyan-50 font-semibold mb-5">Welcome!</h1>
+                <h2 class="text-xl text-cyan-50 mb-10"> Join us today and enjoy our amazing app!</h2>
+                    <div class="relative flex flex-col items-baseline">
                         @if (Route::has('login'))
                         <livewire:welcome.navigation />
                         @endif
